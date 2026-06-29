61a091aec91b244ddecad3f649337882022d1759
Wanxuan Li <t378li@uwaterloo.ca>
2026-06-29 00:07:46 -0400

    8 spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,7 +5,7 @@
 vim.opt.clipboard = "unnamedplus"
 
 -- Use 4 spaces for tabs
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
+vim.o.shiftwidth = 8
+vim.o.tabstop = 8
+vim.o.softtabstop = 8
 vim.o.expandtab = true -- Converts tabs to spaces
